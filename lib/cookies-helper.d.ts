f904d5c23480cfee4f2677807b9a473bcc1edfdf
HoanNN <water102.f1@gmail.com>
2023-08-11 16:24:01 +0700

    update package name

---
+++ lib/cookies-helper.d.ts
@@ -0,0 +1,8 @@
+export declare class CookiesHelper {
+    getItem(sKey: string): string | null;
+    setItem(sKey: string, sValue: string, vEnd: Date | number, sPath: string, sDomain: string, bSecure: boolean): boolean;
+    removeItem(sKey: string, sPath: string, sDomain: string): boolean;
+    hasItem(sKey: string): boolean;
+    keys(): string[];
+}
+//# sourceMappingURL=cookies-helper.d.ts.map
